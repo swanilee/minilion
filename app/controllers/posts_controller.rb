508af7fcfcdf52421b1dac3068b9c5d0b4dcd675
swanilee <ssunkyung00@naver.com>
2017-06-28 11:09:37 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  
+  def oriindex
+  end
+  
+  def index
+  @posts=Post.all
+  end
+  
+  def show
+    @post=Post.find(params[:post_id])
+    @comments=Comment.where(:post_id => params[:post_id])
+  end
+  
+  def edit
+    @post=Post.find(params[:post_id])
+  end
+  
+  def update
+    @post=Post.find(params[:post_id])
+   @post.title=params[:input_title]
+    @post.content=params[:input_content]
+    @post.save
+    redirect_to "/posts/show/#{@post.id}"
+  end
+  
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @post.destroy
+    redirect_to'/posts/index'
+    
+  end
+  
+  def new
+  end
+  
+  def confirm
+  end
+  
+  
+  def create
+  @post = Post.new
+  @post.title =params[:input_title]
+  @post.content =params[:input_content]
+  @post.save
+  redirect_to '/posts/confirm'
+  end
+end
---
+++ db/migrate/20170628060930_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :content
+      #t.integer :post_id 
+      t.belongs_to :post
+      t.timestamps null: false
+    end
+  end
+end
